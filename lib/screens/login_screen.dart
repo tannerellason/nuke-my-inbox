8b06fe1ed181dafae2c9930f6022ea3cda2ab228
ImperfectGrammar <ellason.tanner@gmail.com>
2024-06-30 22:12:10 -0500

    10:12

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,7 +11,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome to Nuke My Inbox v0.8'),
+        title: const Text('Welcome to Nuke My Inbox v0.1'),
         centerTitle: true,
       ),
       body: Column(
